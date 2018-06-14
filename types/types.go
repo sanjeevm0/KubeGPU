8f063725af7669043fd6bb4aeaf121cd8d47fa70
sanjeevm0 <sanjeevm@microsoft.com>
2018-06-14 12:23:04 -0700

    progress towards plugin implementation for devices

--- types/types.go
+++ types/types.go
@@ -115,27 +115,6 @@ func NewNodeInfoWithName(name string) *NodeInfo {
 	return node
 }
 
-type Volume struct {
-	Name   string
-	Driver string
-}
-
-// Device is a device to use
-type Device interface {
-	// New creates the device and initializes it
-	New() error
-	// Start logically initializes the device
-	Start() error
-	// UpdateNodeInfo - updates a node info structure by writing capacity, allocatable, used, scorer
-	UpdateNodeInfo(*NodeInfo) error
-	// Allocate attempst to allocate the devices
-	// Returns list of (VolumeName, VolumeDriver), and list of Devices to use
-	// Returns an error on failure.
-	Allocate(*PodInfo, *ContainerInfo) ([]Volume, []string, error)
-	// GetName returns the name of a device
-	GetName() string
-}
-
 type PredicateFailureReason interface {
 	GetReason() string
 	GetInfo() (ResourceName, int64, int64, int64)
